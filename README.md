0225a4613187c4d572ae0ce066f0372f842bb4e5
blackcat <ok@lidgroupp.ru>
2020-10-27 00:13:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# sslchecker-go
+Test simple project for part monitoring ssl certificate 
+
+
+go run sslchecker.go -cert=STAR_pfdo_ru.crt    
+{   
+    "not_before": "2020-06-28 03:00:00 +0300 MSK",
+    "not_after": "2021-06-30 02:59:59 +0300 MSK",
+    "serial_number": "238347781779472747230435998850405071408",
+    "subject": "*.cyberbe.ru",
+    "issuer": "CN=GlobeSSL DV CA,O=CentralNic Luxembourg Sàrl,C=LU"
+}   
+
---
+++ sslchecker.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+// validity allows unmarshaling the certificate validity date range
+type CertInfo struct {
+	NotBefore    string `json:"not_before"`
+	NotAfter     string `json:"not_after"`
+	SerialNumber string `json:"serial_number"`
+	Subject      string `json:"subject"`
+	Issuer       string `json:"issuer"`
+}
+
+func main() {
+	var certFileName = flag.String("cert","cert.pem", "a string")
+	flag.Parse()
+	pemData, err := ioutil.ReadFile(*certFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, rest := pem.Decode([]byte(pemData))
+	if block == nil || len(rest) > 0 {
+		log.Fatal("Certificate decoding error")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+	buf.Grow(4096) // 4KiB should be enough
+
+	info := CertInfo{
+		SerialNumber: fmt.Sprintf("%s", cert.SerialNumber),
+		NotBefore:    cert.NotBefore.Local().String(),
+		NotAfter:     cert.NotAfter.Local().String(),
+		Subject:      cert.Subject.CommonName,
+		Issuer:       cert.Issuer.String(),
+	}
+
+	prettyJSON, err := json.MarshalIndent(info, "", "    ")
+	if err != nil {
+		log.Fatal("Failed to generate json", err)
+	}
+	fmt.Printf("%s\n", string(prettyJSON))
+
+}
